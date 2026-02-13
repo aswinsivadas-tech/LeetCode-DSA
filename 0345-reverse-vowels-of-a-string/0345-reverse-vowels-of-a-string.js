5aca434aab52edac34eb2cc00aa26e137bc889d3
Aswin Sivadas <aswinsivadas.tech@gmail.com>
2026-02-13 10:22:25 +0530

    Time: 8 ms (52%), Space: 60.1 MB (65.16%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
+    let arr = s.split('');
+    
+    let left = 0;
+    let right = arr.length - 1;
+    
+    while (left < right) {
+        // move left until a vowel is found
+        while (left < right && !vowels.has(arr[left])) {
+            left++;
+        }
+        
+        // move right until a vowel is found
+        while (left < right && !vowels.has(arr[right])) {
+            right--;
+        }
+        
+        // swap vowels
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        
+        left++;
+        right--;
+    }
+    
+    return arr.join('');
+};
