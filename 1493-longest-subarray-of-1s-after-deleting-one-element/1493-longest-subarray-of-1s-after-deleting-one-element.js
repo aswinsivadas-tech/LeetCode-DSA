b9e114e17f26ecf4ba2234e935f4cc8d964bf6da
Aswin Sivadas <aswinsivadas.tech@gmail.com>
2026-02-19 10:44:13 +0530

    Time: 1 ms (92.3%), Space: 61.8 MB (8.69%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function(nums) {
+    let left = 0;
+    let zeroCount = 0;
+    let maxLen = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) {
+            zeroCount++;
+        }
+
+        // shrink window if more than one zero
+        while (zeroCount > 1) {
+            if (nums[left] === 0) {
+                zeroCount--;
+            }
+            left++;
+        }
+
+        // window size
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+
+    // must delete one element
+    return maxLen - 1;
+};
