24950af1925b49ca393eb9c8c0645d8aa35055a9
Aswin Sivadas <aswinsivadas.tech@gmail.com>
2026-02-24 10:42:00 +0530

    Time: 2 ms (99.72%), Space: 104.6 MB (5.97%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -0,0 +1,17 @@
+var deleteMiddle = function(head) {
+    if (!head || !head.next) return null;
+
+    let slow = head;
+    let fast = head;
+    let prev = null;
+
+    while (fast && fast.next) {
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    prev.next = slow.next;
+
+    return head;
+};
