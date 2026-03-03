e17d6c342ddc279e0087fae0298bacb22b4e9593
Aswin Sivadas <aswinsivadas.tech@gmail.com>
2026-03-03 10:41:01 +0530

    Time: 51 ms (88%), Space: 65.2 MB (40%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,13 @@
+var lowestCommonAncestor = function(root, p, q) {
+    if (root === null) return null;
+    if (root === p || root === q) return root;
+
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+
+    if (left !== null && right !== null) {
+        return root;
+    }
+
+    return left !== null ? left : right;
+};
