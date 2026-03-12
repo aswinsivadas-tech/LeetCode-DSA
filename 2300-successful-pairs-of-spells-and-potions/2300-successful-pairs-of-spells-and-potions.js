53ff0ed45bd48e63516bd82b9b363f7a002240dc
Aswin Sivadas <aswinsivadas.tech@gmail.com>
2026-03-12 10:53:11 +0530

    Time: 105 ms (87.5%), Space: 85 MB (85%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
@@ -0,0 +1,29 @@
+var successfulPairs = function(spells, potions, success) {
+    potions.sort((a, b) => a - b);
+    
+    const m = potions.length;
+    const result = [];
+
+    for (let spell of spells) {
+        let left = 0;
+        let right = m - 1;
+        let index = m;
+
+        let minPotion = Math.ceil(success / spell);
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if (potions[mid] >= minPotion) {
+                index = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        result.push(m - index);
+    }
+
+    return result;
+};
