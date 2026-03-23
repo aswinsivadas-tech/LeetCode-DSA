8f721ae38667b7fb4d8880d3573ff3133c55bf83
Aswin Sivadas <aswinsivadas.tech@gmail.com>
2026-03-23 11:10:08 +0530

    Time: 58 ms (6.93%), Space: 55.4 MB (5.94%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var minFlips = function(a, b, c) {
+    let flips = 0;
+
+    for (let i = 0; i < 32; i++) {
+        let bitA = (a >> i) & 1;
+        let bitB = (b >> i) & 1;
+        let bitC = (c >> i) & 1;
+
+        if (bitC === 1) {
+            // Need at least one 1
+            if (bitA === 0 && bitB === 0) {
+                flips += 1;
+            }
+        } else {
+            // Need both 0
+            if (bitA === 1 && bitB === 1) {
+                flips += 2;
+            } else if (bitA === 1 || bitB === 1) {
+                flips += 1;
+            }
+        }
+    }
+
+    return flips;
+};
