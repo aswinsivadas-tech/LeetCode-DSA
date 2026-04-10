1efd904b6757548b8c7a01eb928298b63e8df768
Aswin Sivadas <aswinsivadas.tech@gmail.com>
2026-04-10 13:02:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,7 +1,19 @@
 const isAnagram = function(s, t) {
-    s = s.split("").sort().join("")
-     t = t.split("").sort().join("")
+    if(s.length !== t.length) return false;
 
-     return s===t;
+    let obj1 ={};
+    let obj2 ={};
 
+    for( let i = 0; i < s.length; i++) {
+        obj1[s[i]] = (obj1[s[i]] || 0)+1;
+        obj2[t[i]] = (obj2[t[i]] || 0)+1;
+    }
+
+    for (const key in obj1) {
+        if(obj1[key] !==obj2[key])  return false;
+    }
+
+    return true;
 };
+
+console.log(isAnagram("anagram","nagram"));
