148efc202ea285a0413832bc52bf95bccdb95435
Aswin Sivadas <aswinsivadas.tech@gmail.com>
2026-04-24 11:19:53 +0530

    Time: 1 ms (88.68%), Space: 56.1 MB (78.25%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,18 @@
+var majorityElement = function(nums) {
+    let candidate = null;
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        if (num === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+};
