9d7addfb8e71b04e932a6c420a8442ff19bb9ee7
Aswin Sivadas <aswinsivadas.tech@gmail.com>
2026-04-24 11:23:08 +0530

    Time: 6 ms (16.1%), Space: 66.1 MB (14.26%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,15 @@
+var maxProfit = function(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let price of prices) {
+        if (price < minPrice) {
+            minPrice = price;
+        } else {
+            let profit = price - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+    }
+
+    return maxProfit;
+};
